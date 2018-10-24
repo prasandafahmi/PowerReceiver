3a1f1c9da915da07c0acaefab9012ba8cdd1cf5a
prasandafahmi <fahmiprasanda@unpak.ac.id>
2018-10-24 09:49:30 +0700

    PowerReceiver

---
+++ app/src/main/java/id/computerscience/fahmiprasanda/powerreceiver/CustomReceiver.java
@@ -0,0 +1,46 @@
+package id.computerscience.fahmiprasanda.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+public class CustomReceiver extends BroadcastReceiver {
+
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (intentAction != null) {
+            String toastMessage = context.getString(R.string.unknown_action);
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = context.getString(R.string.power_connected);
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage =
+                            context.getString(R.string.power_disconnected);
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage =
+                            context.getString(R.string.custom_broadcast_toast);
+                    break;
+                case Intent.ACTION_HEADSET_PLUG:
+                    toastMessage =
+                            context.getString(R.string.headset_on);
+                    break;
+                case Intent.ACTION_AIRPLANE_MODE_CHANGED:
+                    toastMessage =
+                            context.getString(R.string.airplane_mode);
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
